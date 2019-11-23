7e428185c5381b603f0eb3309db61fa769337a4f
Oyesola Ogundele <oyekunmi@gmail.com>
2019-11-23 06:53:26 +0400

    wallet: integrate paystack to transaction verifcation

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\WalletTransaction;
+use GuzzleHttp\Client;
 
 class WalletController extends BaseController
 {
@@ -25,15 +26,40 @@ class WalletController extends BaseController
 
     public function verifyTransaction(string $reference)
     {
+
+        $client = new Client();
+        $url = 'https://api.paystack.co/transaction/verify/946778697' ;
+        $response = null;
+        try{
+
+            $response = $client->request('GET', $url, [
+                'headers' => [
+                    'Authorization' => 'Bearer sk_test_a0d0725ffd8124018859083fe04d77eeec41407f'
+                ]
+            ]);
+
+        }catch(\Exception $ex){
+            return $this->_failedPaymentVerification();
+        }
+
+        $result = \json_decode((string) $response->getBody());
+        if(!$result->status){
+            return $this->_failedPaymentVerification();
+        }
+
         $wallet = auth()->user()->wallet;
         WalletTransaction::create([
             'wallet_id' => $wallet->id,
             'transaction_type' => 'CREDIT',
-            'amount' => 10,
+            'amount' => $result->data->amount,
             'wallet_type' => 'CASH',
-            'description' => 'Cash topup',
-            'reference' => $reference
+            'description' => 'Game live purchase',
+            'reference' => $result->data->reference,
         ]);
         return $this->sendResponse(true, 'Payment was successful');
+    }
+
+    private function _failedPaymentVerification(){
+        return $this->sendResponse(false, 'Payment could not be verified. Please wait for your balance to reflect.');
     }
 }
