413eeb709668468e758d0fe492c719d34f3b50c8
Oyesola Ogundele <oyekunmi@yahoo.com>
2020-03-23 21:28:05 +0400

    more performance enhancements

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -25,5 +26,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        DB::listen(function ($query) {
+            dump($query->sql);
+            // $query->bindings
+            // $query->time
+        });
     }
 }
