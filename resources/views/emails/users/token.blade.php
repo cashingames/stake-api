3f568ba700f7d9353473a5ff474a9f1fb03861c4
Zubby <zubbybrightson@gmail.com>
2020-03-25 06:47:31 +0100

    design-email-template

--- resources/views/emails/users/token.blade.php
+++ resources/views/emails/users/token.blade.php
@@ -5,56 +5,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <style>
-        body{
-            background-color: #fff;
-            font-size: 20px;
-            display: flex;
-            justify-content: center;
-            font-family: 'Times New Roman', Times, serif;
-            text-align: justify;
-        }
+
+    <body> <table cellspacing="0" cellpadding="0" border="0" style="color: #333; background: #fff; padding: 0; margin: 0; width: 100%; font: 15px 'Helvetica Neue', Arial, Helvetica;"> <tbody><tr width="100%"> <td valign="top" align="left" style="background: #f0f0f0; font: 15px 'Helvetica Neue', Arial, Helvetica;"> <table style="border: none; padding: 0 18px; margin: 50px auto; width: 500px;"> <tbody>
+
+        <tr width="100%" height="57"> <td valign="top" align="left" style="border-top-left-radius: 4px; border-top-right-radius: 4px; background: #0079BF; padding: 12px 18px; text-align: center;">
        
-        
-        .card {
-            margin-top: 50px;
-            box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-            width: 350px;
-        }
-        
-        h3 {
-            color: white;
-            display:flex;
-            justify-content: center;
-        }
-
-        .heading{
-            background-color: #4361f6;
-            padding: 2px;
-            border-radius: 2px;
-        }
-
-        .container{
-            padding: 2px 16px;
-            margin-top: 5px;
-        }
-        
-    </style>
+        <img height="37" width="122" src="https://stg.cashingames.com/images/logo.png" title="Cashingames" style="font-weight: bold; font-size: 18px; color: #fff; vertical-align: top;"> </td> </tr>
+       
+        <tr width="100%"> <td valign="top" align="left" style="border-bottom-left-radius: 4px; border-bottom-right-radius: 4px; background:#fff; padding: 18px;"> <h1 style="font-size: 20px; margin: 0; color: #333;"> Hello There!! </h1>
+       
+        <p style="font: 15px/1.25em 'Helvetica Neue', Arial, Helvetica; color: #333;"> We heard you need a password reset. To reset your password, please use this code <b> {{$token}} </b> . </p>
+       
+        <p style="font: 15px/1.25em 'Helvetica Neue', Arial, Helvetica; margin-bottom: 0; text-align: center; color: #333;">
+        </p>
+       
+        <p style="font: 15px/1.25em 'Helvetica Neue', Arial, Helvetica; color: #939393; margin-bottom: 0;"> If you didn't try to reset your password, please ignore this email. </p>
+       
+        </td> </tr>
+       
+        </tbody> </table> </td> </tr></tbody> </table> 
+       </body> 
     
-</head>
-<body>
-    <div class = "card">
-        <div class= "heading">
-            <h3> Recover Your Password </h3>    
-        </div>
-        <div class= "container">
-            <span><b> Hello There !!</b></span>
-            <p>You requested for a password reset!. 
-            To reset your password, please use this code <b> {{$token}} .</b> </p>
-            <span> If you did not request for a reset, please ignore this email.</span>
-        </div>
-    </div>
-</body>
 </html>
+
+
 
 
