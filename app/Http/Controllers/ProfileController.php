fa5d39f972a6c7c5967a4189b0a4738a72b41e08
Zubby <zubbybrightson@gmail.com>
2020-05-25 15:02:16 +0100

    edit profile endpoint modification

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -51,17 +51,31 @@ class ProfileController extends BaseController
         }
 
 
+
         $profile->first_name = $data['firstName'];
         $profile->last_name = $data['lastName'];
-        $profile->gender =  $data['gender'];
-        $profile->date_of_birth = new Carbon($data['dateOfBirth']);
-        $profile->address = $data['address'];
-        $profile->state = $data['state'];
         $profile->avatar = $profile->avatar;
         $profile->account_name = $data['accountName'];
         $profile->bank_name = $data['bankName'];
         $profile->account_number = $data['accountNumber'];
         $profile->currency = $data['currency'];
+
+        if(isset($data['gender']) &&  !is_null($data['gender'])){
+            $profile->gender =  $data['gender'];
+        }
+
+        if(isset($data['dateOfBirth']) && !is_null($data['dateOfBirth'])){
+            $profile->date_of_birth = new Carbon($data['dateOfBirth']) ;
+        }
+
+        if(isset($data['address']) && !is_null($data['address'])){
+            $profile->address = $data['address'] ;
+        }
+
+        if(isset($data['state']) && !is_null($data['state'])){
+            $profile->state = $data['state'] ;
+        }
+
         $profile->save();
 
         $user->update([
