99ec8210a16df8fb0ec29e25bf553e4bef8db02f
Zubby <zubbybrightson@gmail.com>
2020-05-25 14:41:03 +0100

    include nullable avatar field in edit profile payload

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -39,6 +39,7 @@ class ProfileController extends BaseController
             'bankName' => ['nullable', 'string', 'max:255'],
             'accountNumber' => ['nullable', 'string', 'max:255'],
             'currency' => ['nullable', 'string', 'max:100'],
+            'avatar' => ['nullable', 'string', 'max:1000']
         ]);
 
 
