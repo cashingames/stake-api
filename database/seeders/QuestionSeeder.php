8aff508973f4b3736f56cf658a11791425a30b7f
Oyesola Ogundele <oyekunmi@yahoo.com>
2020-09-25 07:08:39 -0400

    seeding: improve seeding code

--- database/seeders/QuestionSeeder.php
+++ database/seeders/QuestionSeeder.php
@@ -11,6 +11,7 @@ use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
 class QuestionSeeder extends Seeder
 {
+  private $categoryName = 'Music';
   /**
    * Run the database seeds.
    *
@@ -18,7 +19,7 @@ class QuestionSeeder extends Seeder
    */
   public function run()
   {
-    if ( env('APP_ENV') == 'testing' || env('APP_ENV') == 'local' ) {
+    if ( env('APP_ENV') == 'testing' ) {
       Question::factory()->times(50)->create()->each(function ($question) {
         $options = Option::factory()->times(4)->make();
         $option = $options->random();
@@ -37,48 +38,91 @@ class QuestionSeeder extends Seeder
         );
       });
     } else {
-      $inputFileName = base_path('Music.xlsx');
+      $inputFileName = base_path($this->categoryName.'.xlsx');
       $reader = IOFactory::createReader('Xlsx');
       $reader->setReadDataOnly(TRUE);
       $spreadsheet = $reader->load($inputFileName);
-
-      $workSheet  = $spreadsheet->getActiveSheet();
-
-      $this->readWorkSheet($workSheet);
+      
+      foreach ($spreadsheet->getAllSheets() as $currentSheet  ) {
+         
+        $this->readWorkSheet($currentSheet);
+        
+      }
+      
     }
   }
 
-  private function readWorkSheet(Worksheet $workSheet)
-  {
-    $categoryName = $workSheet->getTitle();
-    $category = Category::where('name', $categoryName)->first();
-
-    if ($category == null) {
+  private function readWorkSheet(Worksheet $workSheet){
+   
+    $category = Category::where('name', $this->categoryName)->first();
+    if($category==null){
+      echo "Category cannot be found \n";
       return;
     }
+    
+    $highestRow = $workSheet->getHighestRow(); // e.g. 10
+    for ($i = 2; $i <= $highestRow; $i++) {
 
-    for ($i = 2; $i < 500; $i++) {
-      $question = new Question;
-
-      $level = $workSheet->getCellByColumnAndRow(1, $i)->getValue();
-      if (!isset($level) || trim($level) === '') {
+      $level = trim($workSheet->getCellByColumnAndRow(1, $i)->getValue());
+      if ($level == '' || ctype_space($level)) {
+        echo "Row ".$i." level is empty \n";
         continue;
       }
 
-      $question->level = $level;
-      $question->label = $workSheet->getCellByColumnAndRow(2, $i)->getValue();
-      $question->category_id = $category->id;
-      $question->save();
-
-      for ($j = 3; $j <= 6; $j++) {
-        $option = new  Option();
-        $option->title = $workSheet->getCellByColumnAndRow($j, $i)->getValue();
-        $option->question_id = $question->id;
-        $option->is_correct = $option->title == $workSheet->getCellByColumnAndRow(7, $i)->getValue();
-        $option->save();
+      $label = trim($workSheet->getCellByColumnAndRow(2, $i)->getValue());
+      if ($label == '' || ctype_space($label)) {
+        echo "Row ".$i." question is empty \n";
+        continue;
       }
 
-    }
+      $answer = trim($workSheet->getCellByColumnAndRow(7, $i)->getValue());
+      if ($answer == '' || ctype_space($answer)) {
+        echo "Row ".$i." answer is empty \n";
+        continue;
+      }
+
+      $question = new Question;
+      $question->level = strtolower($level);
+      $question->label = $label;
+      $question->category_id = $category->id;
+
+      $options = [];
+      $hasCorrectAnswer = false;
+
+      for ($j = 3; $j <= 6; $j++) {
+        $optionLabel= trim($workSheet->getCellByColumnAndRow($j, $i)->getValue());
+        
+        if( ctype_space($optionLabel) || $optionLabel=='' ){
+          // echo "Option on R".$i."C".$j." is empty \n";
+          continue;
+        }
+
+        $isCorrect = $optionLabel == $answer;
+        if($isCorrect){
+          $hasCorrectAnswer = true;
+        }
+
+        $option = new  Option();
+        $option->title = $optionLabel;
+        $option->is_correct = $isCorrect;
+
+        if($option)
+
+        array_push($options, $option);
+      }
+
+      if($hasCorrectAnswer){
+        \DB::transaction(function () use ($question, $options) {
+          $question->save();
+          $question->options()->saveMany($options);
+        });
+      }else{
+        echo "R".$i." does not have a correct answer \n";
+        continue;
+      }
+
+
+    }    
 
   }
 }
