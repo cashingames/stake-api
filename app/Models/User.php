8f3efb75d90916c48014a1acf578327910a1a411
Zubby <zubbybrightson@gmail.com>
2021-07-16 11:22:26 +0100

    set password attribute

--- app/Models/User.php
+++ app/Models/User.php
@@ -71,5 +71,11 @@ class User extends Authenticatable implements JWTSubject
     {
         return [];
     }
+    public function setPasswordAttribute($password)
+    {
+        if (!empty($password)) {
+            $this->attributes['password'] = bcrypt($password);
+        }
+    }
 
 }
