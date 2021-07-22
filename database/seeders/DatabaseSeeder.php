9c7d4512e9300d141a4f81b12e0623c1184fbb25
Zubby <zubbybrightson@gmail.com>
2021-07-22 14:00:09 +0100

    WIP/BoostSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             UserSeeder::class,
-            PointSeeder::class,
+            // PointSeeder::class,
             CategorySeeder::class
         ]);
     }
