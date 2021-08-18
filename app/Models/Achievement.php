8b3d2b907c4f234e8703f3c9f7cb9b2e09982542
Zubby <zubbybrightson@gmail.com>
2021-08-18 19:35:36 +0100

    return is claimed when user claims an achievement

--- app/Models/Achievement.php
+++ app/Models/Achievement.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Achievement extends Model
 {
@@ -11,8 +12,27 @@ class Achievement extends Model
 
     protected $fillable = ['title', 'milestone', 'medal'];
 
+    protected $appends = [
+       'is_claimed'
+    ];
+    
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function getIsClaimedAttribute()
+    {   
+        $user = auth()->user();
+
+        $userAchievement = DB::table('user_achievements')
+        ->where('achievement_id',$this->id)
+        ->where('user_id', $user->id)->first();
+        
+        if($userAchievement === null){
+           return false;
+        }else{
+            return true;
+        }
     }
 }
