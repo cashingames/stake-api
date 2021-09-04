fb5e52e76e5d323d600684cf56d76b4199b14082
Zubby <zubbybrightson@gmail.com>
2021-09-04 19:47:21 +0100

    add is available to game type

--- app/Models/GameType.php
+++ app/Models/GameType.php
@@ -11,8 +11,22 @@ class GameType extends Model
 
     protected $fillable = ['name','description', 'instruction','category_id'];
 
+    protected $appends = [
+        'is_available'
+     ];
+
     public function category(){
         return $this->belongsTo(Category::class);
     }
 
+    public function getisAvailableAttribute()
+    {   
+        $hasQuestions = Question::where('game_type_id',$this->id)->first();
+        
+        if($hasQuestions === null){
+            return false;
+        }
+        return true;
+
+    }
 }
--- database/seeders/GameTypeSeeder.php
+++ database/seeders/GameTypeSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Carbon;
 
 class GameTypeSeeder extends Seeder
 {
@@ -20,6 +21,8 @@ class GameTypeSeeder extends Seeder
             [
                 'name' => "True or False",
                 'description' => "Select from two options whether true or false",
+                'created_at'=> Carbon::now(),
+                'updated_at'=> Carbon::now()
             ]
         );
 
@@ -27,6 +30,8 @@ class GameTypeSeeder extends Seeder
             [
                 'name' => "Multi Choice",
                 'description' => "Select one correct answer from other options",
+                'created_at'=> Carbon::now(),
+                'updated_at'=> Carbon::now()
             ]
         );
 
