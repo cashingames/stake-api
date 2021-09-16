67c77a783141a88277991c9ba2a8007a069b84a4
Zubby <zubbybrightson@gmail.com>
2021-09-16 15:59:52 +0100

    add notifications

--- database/migrations/2021_09_16_121218_add_columns_to_notifications_table.php
+++ database/migrations/2021_09_16_121218_add_columns_to_notifications_table.php
@@ -15,7 +15,7 @@ class AddColumnsToNotificationsTable extends Migration
     {
         Schema::table('notifications', function (Blueprint $table) {
             //
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->nullable()->constrained()->onDelete('cascade');
             $table->string('title');
             $table->string('message');
             $table->boolean('is_read')->default(false);
