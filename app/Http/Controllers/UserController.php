f10aaf2e6fc364661f96a9568e657f026312eadb
Zubby <zubbybrightson@gmail.com>
2021-10-07 18:44:50 +0100

    get offline friends

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -96,7 +96,7 @@ class UserController extends BaseController
 
         $result = [
             'online' => $onlineFriends,
-            'offline' => array_diff($offlineFriends, $onlineFriends)
+            'offline' => $offlineFriends
         ];
         return $this->sendResponse($result, "Friends");
     }
