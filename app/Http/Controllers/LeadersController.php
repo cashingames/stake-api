767abf1632fc3891494d7702248d0d326a80a3e5
Oyesola Ogundele <oyekunmi@yahoo.com>
2022-02-11 17:18:57 +0400

    adding timerange to leaderboard endpoint

--- app/Http/Controllers/LeadersController.php
+++ app/Http/Controllers/LeadersController.php
@@ -12,21 +12,29 @@ class LeadersController extends BaseController
      * Returns just the global leaders
      * This is useful for dashboard where we only need global leaders
      */
-    public function global()
+    public function global($startDate = null, $endDate = null)
     {
+        if ($startDate && $endDate) {
+            $_startDate = Carbon::createFromTimestamp($startDate)->startOfDay();
+            $_endDate = Carbon::createFromTimestamp($endDate)->tomorrow();
+        } else {
+            $_startDate = today();
+            $_endDate = Carbon::tomorrow();
+        }
+
         $leaders = DB::select(
             'SELECT g.points, p.avatar, p.first_name , p.last_name, g.username
             FROM (
                 SELECT SUM(points_gained) AS points, user_id, username FROM game_sessions gs
                 INNER JOIN users ON users.id = gs.user_id
-                WHERE DATE(gs.created_at) = ?
+                WHERE gs.created_at >= ? AND gs.created_at < ?
                 GROUP BY user_id
                 ORDER BY points DESC
                 LIMIT 25
             ) g
             INNER JOIN profiles p ON g.user_id = p.user_id
             ORDER BY g.points DESC',
-            [now()->toDateString()]
+            [$_startDate,  $_endDate]
         );
 
         return $this->sendResponse($leaders, "Global Leaders");
@@ -36,8 +44,16 @@ class LeadersController extends BaseController
      * Returns all the leaders for the categories in the system
      * This can be useful in the future for anything else we need to implement
      */
-    public function categories()
+    public function categories($startDate = null, $endDate = null)
     {
+        if ($startDate && $endDate) {
+            $_startDate = Carbon::createFromTimestamp($startDate)->startOfDay();
+            $_endDate = Carbon::createFromTimestamp($endDate)->tomorrow();
+        } else {
+            $_startDate = today();
+            $_endDate = Carbon::tomorrow();
+        }
+
         $response = [];
         $leaders = DB::select(
             'SELECT p.avatar, p.first_name, p.last_name, r.points, c.id as category_id, c.name as category_name, r.username
@@ -46,7 +62,7 @@ class LeadersController extends BaseController
                 FROM game_sessions gs
                 INNER JOIN categories c on c.id = gs.category_id
                 INNER JOIN users u on u.id = gs.user_id
-                WHERE DATE(gs.created_at) = ?
+                WHERE gs.created_at >= ? AND gs.created_at < ?
                 group by c.category_id, gs.user_id
                 order by points desc 
                 limit 25
@@ -54,7 +70,7 @@ class LeadersController extends BaseController
             join profiles p on p.user_id = r.user_id
             join categories c on c.id = r.category_id
             order by r.points desc',
-            [now()->toDateString()]
+            [$_startDate,  $_endDate]
         );
         foreach ($leaders as $leader) {
             $response[$leader->category_name][] = $leader;
