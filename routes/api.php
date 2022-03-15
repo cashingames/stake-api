cef08911ea852819883dc91104179c86b2d251db
Zubby <zubbybrightson@gmail.com>
2022-03-15 11:31:00 +0100

    add additional api for filtering leaders

--- routes/api.php
+++ routes/api.php
@@ -106,9 +106,9 @@ Route::prefix('v2')->group(function () {
         Route::post('game/boost/consume/{boostId}', [GameController::class, 'consumeBoost']);
 
         Route::post('game/challenge/invite', [GameController::class, 'sendChallengeInvite']);
-        // Route::get('leaders/global', [LeadersController::class, 'global']);
+        Route::get('leaders/global', [LeadersController::class, 'global']);
         Route::get('leaders/global/{startDate}/{endDate}', [LeadersController::class, 'global']);
-        // Route::get('leaders/categories', [LeadersController::class, 'categories']);
+        Route::get('leaders/categories', [LeadersController::class, 'categories']);
         Route::get('leaders/categories/{startDate}/{endDate}', [LeadersController::class, 'categories']);
 
 
