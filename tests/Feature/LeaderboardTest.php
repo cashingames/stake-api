86917aa01c44b34ecba786f9f801956d1832810e
Zubby <zubbybrightson@gmail.com>
2022-04-04 19:03:31 +0100

    Leaderboard test cases

---
+++ tests/Feature/LeaderboardTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\GameSession;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use UserSeeder;
+use App\Models\User;
+use Carbon\Carbon;
+
+class LeaderboardTest extends TestCase
+{   
+    use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+
+    const GLOBAL_LEADERS_URL = '/api/v2/leaders/global/';
+    const CATEGORIES_LEADERS_URL = '/api/v2/leaders/categories/';
+    protected $user;
+
+    protected function setUp(): void{
+        parent::setUp();
+        
+        $this->seed(UserSeeder::class);
+        $this->user = User::first(); 
+
+        $this->actingAs($this->user);
+
+    }
+
+    public function test_global_leaderboard_should_return_data_since_inception_if_no_filter_date_is_passed(){
+   
+        $response = $this->get(self::GLOBAL_LEADERS_URL);
+        $response->assertStatus(200);
+    }
+
+    public function test_global_leaderboard_should_return_data_based_on_date_range_if_date_range_filter_is_passed(){
+     
+        $startDate = Carbon::today()->subDays(rand(0, 365));
+        $endDate = Carbon::today()->addDays(rand(0, 365));
+
+        $response = $this->get(self::GLOBAL_LEADERS_URL.$startDate.'/'.$endDate);
+        $response->assertStatus(200);
+    }
+
+    public function test_categories_leaderboard_should_return_data_since_inception_if_no_filter_date_is_passed(){
+     
+
+        $response = $this->get(self::CATEGORIES_LEADERS_URL);
+        $response->assertStatus(200);
+    }
+
+    public function test_categories_leaderboard_should_return_data_based_on_date_range_if_date_range_filter_is_passed(){
+     
+        $startDate = Carbon::today()->subDays(rand(0, 365));
+        $endDate = Carbon::today()->addDays(rand(0, 365));
+
+        $response = $this->get(self::CATEGORIES_LEADERS_URL.$startDate.'/'.$endDate);
+        $response->assertStatus(200);
+    }
+}
