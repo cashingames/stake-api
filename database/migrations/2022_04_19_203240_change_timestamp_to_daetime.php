9ed3addd764d411ee66368bcf5dcd47fd5cf643c
Zubby <zubbybrightson@gmail.com>
2022-04-19 21:48:11 +0100

    change start time and endtime column type in trivias

---
+++ database/migrations/2022_04_19_203240_change_timestamp_to_daetime.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('trivias', function (Blueprint $table) {
+            //
+            $table->dropColumn('start_time');
+            $table->dropColumn('end_time');
+
+           
+        });
+
+        Schema::table('trivias', function (Blueprint $table){
+            $table->dateTime('start_time');
+            $table->dateTime('end_time');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('trivias', function (Blueprint $table) {
+            //
+            $table->dropColumn('start_time');
+            $table->dropColumn('end_time');
+
+            // $table->timestamp('start_time');
+            // $table->timestamp('end_time');
+        });
+    }
+};
