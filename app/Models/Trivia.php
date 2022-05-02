f19b41f97fec7572b124d25c523f3336af82fd28
Zubby <zubbybrightson@gmail.com>
2022-05-02 12:27:00 +0100

    add soft deletes to trivias

--- app/Models/Trivia.php
+++ app/Models/Trivia.php
@@ -5,9 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Carbon;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 class Trivia extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $table = 'trivias';
 
