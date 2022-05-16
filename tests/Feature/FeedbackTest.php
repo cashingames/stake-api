db8e045650d4731557e585bf01af54bdefef9f54
Zubby <zubbybrightson@gmail.com>
2022-05-16 15:27:03 +0100

    additional test cases

--- tests/Feature/FeedbackTest.php
+++ tests/Feature/FeedbackTest.php
@@ -18,16 +18,45 @@ class FeedbackTest extends TestCase
     public function test_a_feedback_can_be_sent()
     {
         Mail::fake();
-        
-        $response = $this->post('/api/v2/client/feedback',[
+
+        $response = $this->post('/api/v2/client/feedback', [
             "first_name" => "Test",
             "last_name" => "User",
             "email" => "email@user.com",
-            "message_body"=> "Lorem Ipsum dorem bla bla bla"
+            "message_body" => "Lorem Ipsum dorem bla bla bla"
         ]);
 
         Mail::assertSent(Feedback::class);
 
         $response->assertStatus(200);
+    }
+
+    public function test_a_feedback_cannot_be_sent_with_empty_fields()
+    {
+        Mail::fake();
+
+        $this->post('/api/v2/client/feedback', [
+            "first_name" => "",
+            "last_name" => "",
+            "email" => "",
+            "message_body" => ""
+        ]);
+
+        Mail::assertNotSent(Feedback::class);
+    }
+
+    public function test_faq_and_answers_can_be_fetched()
+    {
+
+        $response = $this->get('/api/v2/faq/fetch');
+
+        $response->assertJsonStructure([
+            'data' => [
+                '*' => [
+                     'question',
+                     'answer',
+                ]
+            ]
+        ]);
     }
 }
