3f7aecea1a618b3f015f604226fcdd1e4960665e
Zubby Brightson <39646315+zubbybright@users.noreply.github.com>
2022-06-07 17:36:21 +0100

    Date utils unit test (#105)
    * Date Utils Tests
    
    * include to timezone test
    
    * change DateUtilsTest to unit test

---
+++ tests/Unit/DateUtilsTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use App\Traits\Utils\DateUtils;
+use Carbon\Carbon;
+
+class DateUtilsTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+
+    public $trait;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->trait = $this->getObjectForTrait(DateUtils::class);
+
+    }
+
+    public function test_that_date_can_be_converted_to_timezone() {
+      
+        $test = $this->trait->toTimeZone(Carbon::now(),'Africa/Lagos', 'Asia/Baghdad');
+
+        $this->assertEquals(Carbon::parse($test)->timezoneName, 'Asia/Baghdad');
+    }
+
+    public function test_that_date_can_be_converted_to_UTC() {
+      
+        $test = $this->trait->toUtc(Carbon::now(),'Africa/Lagos');
+
+        $this->assertEquals(Carbon::parse($test)->timezoneName, 'UTC');
+    }
+
+    public function test_that_date_can_be_converted_to_Nigerian_Timezone() {
+       
+        $test = $this->trait->toNigerianTimeZone(Carbon::now(),'America/Vancouver');
+
+        $this->assertEquals(Carbon::parse($test)->timezoneName, 'Africa/Lagos');
+    }
+
+    public function test_that_date_can_be_converted_to_UTC_from_Nigerian_timezone() {
+     
+        $test = $this->trait->toUtcFromNigeriaTimeZone(Carbon::now('Africa/Lagos'));
+
+        $this->assertEquals(Carbon::parse($test)->timezoneName, 'UTC');
+    }
+
+    public function test_that_date_can_be_converted_to_Nigerian_timezone_from_UTC() {
+       
+        $test = $this->trait->toNigeriaTimeZoneFromUtc(Carbon::now('UTC'));
+
+        $this->assertEquals(Carbon::parse($test)->timezoneName, 'Africa/Lagos');
+    }
+
+    public function test_that_date_can_be_converted_to_timestamp() {
+       
+        $test = $this->trait->toTimestamp(Carbon::now());
+
+        $this->assertEquals($test/1000, Carbon::now()->timestamp);
+    }
+
+}
