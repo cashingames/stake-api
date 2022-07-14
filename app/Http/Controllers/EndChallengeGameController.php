526a9da6a86d48ae0c8e445a54069e79042d203d
chijibson1 <chichebemjibunoh@gmail.com>
2022-07-14 11:54:26 +0100

    add end game challenge endpoint

---
+++ app/Http/Controllers/EndChallengeGameController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ChallengeGameSession;
+use App\Models\Question;
+use App\Models\UserBoost;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class EndChallengeGameController extends  BaseController
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        Log::info($request->all());
+
+        $game = ChallengeGameSession::where('session_token', $request->token)->first();
+        if ($game === null) {
+            return $this->sendError('Challenge Game Session does not exist', 'Challenge Game Session does not exist');
+        }
+        //@TODO Remove after fixing double submission bug.
+        if ($game->state === "COMPLETED") {
+            return $this->sendResponse($game, 'Challenge Game Ended');
+        }
+        $game->end_time = Carbon::now()->subMilliseconds(500);
+        $game->state = 'COMPLETED';
+        $questions = collect(Question::with('options')->whereIn('id', array_column($request->chosenOptions, 'question_id'))->get());
+        $points = 0;
+        $wrongs = 0;
+        foreach ($request->chosenOptions as $a) {
+            $isCorect = $questions->firstWhere('id', $a['question_id'])->options->where('id', $a['id'])->where('is_correct', base64_encode(true))->first();
+
+            if ($isCorect != null) {
+                $points = $points + 1;
+            } else {
+                $wrongs = $wrongs + 1;
+            }
+        }
+
+        $game->wrong_count = $wrongs;
+        $game->correct_count = $points;
+        $game->points_gained = $points * 5; //@TODO to be revised
+        $game->total_count = $points + $wrongs;
+        $game->save();
+
+        if ($points > 0) {
+            $this->creditPoints($this->user->id, $game->points_gained, "Points gained from challenge game played");
+        }
+
+
+        foreach ($request->consumedBoosts as $row) {
+            $userBoost = UserBoost::where('user_id', $this->user->id)->where('boost_id', $row['boost']['id'])->first();
+
+            $userBoost->update([
+                'used_count' => $userBoost->used_count + 1,
+                'boost_count' => $userBoost->boost_count - 1
+            ]);
+        }
+
+        return $this->sendResponse($game, 'Challenge Game Ended');
+    }
+}
