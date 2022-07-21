56dcac0c03a8dbf639740ac9dd342131c4e26a34
Zubby <zubbybrightson@gmail.com>
2022-07-21 18:46:06 +0100

    restyle verify email blade

--- resources/views/emails/users/verifyEmail.blade.php
+++ resources/views/emails/users/verifyEmail.blade.php
@@ -52,7 +52,7 @@
                         <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'; box-sizing: border-box;">
                             <tr>
                                 <td style="font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'; box-sizing: border-box;">
-                                    <span>Click <a class="button button-primary" style="font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'; box-sizing: border-box; border-radius: 3px; box-shadow: 0 2px 3px rgba(0, 0, 0, 0.16); color: #fff; display: inline-block; text-decoration: none; -webkit-text-size-adjust: none; background-color: #3490dc; border-top: 10px solid #3490dc; border-right: 18px solid #3490dc; border-bottom: 10px solid #3490dc; border-left: 18px solid #3490dc;" href= "{{config('app.url')}}/redirect-verified-email/{{$email}}?xyz={{$email}}"><b>here</b></a> to verify your email.</span>
+                                    <span>Click on <a  href= "{{config('app.url')}}/redirect-verified-email/{{$email}}?xyz={{$email}}"><b>this link</b></a> to verify your account.</span>
                                 </td>
                             </tr>
                         </table>
@@ -62,7 +62,7 @@
         </td>
     </tr>
 </table>
-<p style="font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'; box-sizing: border-box; color: #3d4852; font-size: 16px; line-height: 1.5em; margin-top: 0; text-align: left;">Regards,<br>
+<p style="font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'; box-sizing: border-box; color: #3d4852; font-size: 16px; line-height: 1.5em; margin-top: 0; text-align: left;"><br>
 Cashingames</p>
 
                                     </td>
