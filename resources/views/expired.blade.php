28e65d0fa21125e601520bc0f91189cdc278130a
Zubby <zubbybrightson@gmail.com>
2022-07-26 15:18:34 +0100

    resolve double verifying bug

---
+++ resources/views/expired.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Expired!</title>
+</head>
+
+<body class="antialiased">
+    <div> This link has expired. </div>
+</body>
+
+</html>
