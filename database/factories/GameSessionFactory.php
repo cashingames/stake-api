47acf9ca607a3c23980897937cf671c0be45c6ab
Zubby <zubbybrightson@gmail.com>
2022-07-26 12:14:39 +0100

    leaderboard test

--- database/factories/GameSessionFactory.php
+++ database/factories/GameSessionFactory.php
@@ -27,11 +27,11 @@ class GameSessionFactory extends Factory
     {
         return [
             //
-            'user_id' => User::factory(),
+            'user_id' => $this->faker->randomElement(array(1,2,3,4,5)),
             'plan_id' => $this->faker->randomElement(array(1,2,3,4)),
             'game_mode_id'=> $this->faker->randomElement(array(1,2)),
             'game_type_id' => 2,
-            'category_id' => $this->faker->randomElement(array(101,501)),
+            'category_id' => $this->faker->randomElement(array(102,502)),
             'start_time' => Carbon::now(),
             'end_time' => Carbon::now()->addMinutes(1),
             'session_token' => Str::random(20),
--- tests/Feature/LeaderboardTest.php
+++ tests/Feature/LeaderboardTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 use UserSeeder;
+use Database\Seeders\CategorySeeder;
 use App\Models\GameSession;
 use App\Models\User;
 use Carbon\Carbon;
@@ -22,76 +23,73 @@ class LeaderboardTest extends TestCase
         parent::setUp();
 
         $this->seed(UserSeeder::class);
+        $this->seed(CategorySeeder::class);
         $this->user = User::first();
         GameSession::factory()
-        ->count(20)
-        ->create();
+            ->count(20)
+            ->create();
 
         $this->actingAs($this->user);
     }
 
-    // public function test_global_leaderboard_should_return_data_since_inception_if_no_filter_date_is_passed(){
-
-    //     $response = $this->get(self::GLOBAL_LEADERS_URL);
-
-
-    //     $response->assertJsonCount(5, 'data');
-
-    // }
 
     public function test_global_leaderboard_should_return_data_based_on_date_range_if_date_range_filter_is_passed()
     {
 
-        $startDate = Carbon::today()->subDays(1);
+        $startDate = Carbon::today()->subDays(2);
         $endDate = Carbon::now();
 
-        $response = $this->get(self::GLOBAL_LEADERS_URL . $startDate . '/' . $endDate);
-        $response->assertJsonCount(0, 'data');
+        $response = $this->postjson(self::GLOBAL_LEADERS_URL, [
+            'startDate' => $startDate,
+            'endDate' => $endDate
+        ]);
+        $response->assertJsonCount(5, 'data');
     }
 
-    public function test_categories_leaderboard_should_return_data_since_inception_if_no_filter_date_is_passed()
-    {
-        // $this->seed(CategorySeeder::class);
-        $response = $this->get(self::CATEGORIES_LEADERS_URL);
-
-        $response->assertJsonCount(0, 'data.*.*');
-    }
+    
 
     public function test_categories_leaderboard_should_return_data_based_on_date_range_if_date_range_filter_is_passed()
     {
         // $this->seed(CategorySeeder::class);
 
-        $startDate = Carbon::today()->subDays(1);
+        $startDate = Carbon::today()->subDays(2);
         $endDate = Carbon::now()->addMinute();
 
-        $response = $this->get(self::CATEGORIES_LEADERS_URL . $startDate . '/' . $endDate);
+        $response = $this->post(self::CATEGORIES_LEADERS_URL, [
+            'startDate' => $startDate,
+            'endDate' => $endDate
+        ]);
 
-        $response->assertJsonCount(0, 'data.*.*');
+        $response->assertJsonCount(2, 'data');
     }
 
-    public function test_global_leaders_can_be_fetched_by_post_method_without_dates(){
-       
+    public function test_global_leaders_can_be_fetched_by_post_method_without_dates()
+    {
+
         $response = $this->post(self::GLOBAL_LEADERS_URL);
         $response->assertStatus(200);
     }
 
-    public function test_global_leaders_can_be_fetched_by_post_method_with_dates(){
-       
-        $response = $this->post(self::GLOBAL_LEADERS_URL,[
+    public function test_global_leaders_can_be_fetched_by_post_method_with_dates()
+    {
+
+        $response = $this->post(self::GLOBAL_LEADERS_URL, [
             'startDate' => Carbon::today()->startOfDay('Africa/Lagos'),
             'endDate' => Carbon::tomorrow()->startOfDay('Africa/Lagos')
         ]);
         $response->assertStatus(200);
     }
-    public function test_categories_leaders_can_be_fetched_by_post_method_without_dates(){
-       
+    public function test_categories_leaders_can_be_fetched_by_post_method_without_dates()
+    {
+
         $response = $this->post(self::CATEGORIES_LEADERS_URL);
         $response->assertStatus(200);
     }
 
-    public function test_categories_leaders_can_be_fetched_by_post_method_with_dates(){
-       
-        $response = $this->post(self::CATEGORIES_LEADERS_URL,[
+    public function test_categories_leaders_can_be_fetched_by_post_method_with_dates()
+    {
+
+        $response = $this->post(self::CATEGORIES_LEADERS_URL, [
             'startDate' => Carbon::today()->startOfDay('Africa/Lagos'),
             'endDate' => Carbon::tomorrow()->startOfDay('Africa/Lagos')
         ]);
