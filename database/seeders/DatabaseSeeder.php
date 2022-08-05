a78b0964ce99cb28a5fc990bff6fc49369924e0c
Sodeeq Elusoji <omoelu1@gmail.com>
2022-08-05 11:28:05 +0100

    add sqlite database to gitignore

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     {
 
         $this->call([
-            // CategorySeeder::class,
+            CategorySeeder::class,
             GameTypeSeeder::class,
             GameModeSeeder::class,
             AchievementSeeder::class,
