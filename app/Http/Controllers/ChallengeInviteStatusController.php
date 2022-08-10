3fb56138c9dc3674df068313bafeeeabdd17ec38
Sodeeq Elusoji <omoelu1@gmail.com>
2022-08-10 09:01:32 +0100

    add push notification when challenge status changes

--- app/Http/Controllers/ChallengeInviteStatusController.php
+++ app/Http/Controllers/ChallengeInviteStatusController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\ResponseHelpers\ChallengeDetailsResponse;
-use App\Mail\RespondToChallengeInvite;
-use App\Models\Challenge;
 use App\Models\User;
+use App\Models\Challenge;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
+use App\Actions\SendPushNotification;
+use App\Mail\RespondToChallengeInvite;
+use App\Http\ResponseHelpers\ChallengeDetailsResponse;
 
 class ChallengeInviteStatusController extends BaseController
 {
@@ -31,6 +32,11 @@ class ChallengeInviteStatusController extends BaseController
         Log::info("Challenge with ID: " . $request->challenge_id . "  has been " . $status . " by " . $this->user->username);
 
         Mail::send(new RespondToChallengeInvite($status, $player, $request->challenge_id));
+
+        if (env('PUSH_ENABLED')){
+            $pushAction = new SendPushNotification();
+            $pushAction->sendChallengeStatusChangeNotification($this->user, $player, $updatedChallenge, $status);
+        }
 
         Log::info("Challenge $request->challenge_id  response has been sent from " . $this->user->username);
 
