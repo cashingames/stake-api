216210f793b30f7cea07cdc477f5cf3469f8b27e
Sodeeq Elusoji <omoelu1@gmail.com>
2022-09-13 19:15:38 +0100

    setup feature flagging framework

---
+++ tests/Feature/FeatureFlagTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Services\FeatureFlag;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class FeatureFlagTest extends TestCase
+{
+    /**
+     * @expectedException App\Exceptions\UnknownFeatureException
+     */
+    public function test_throws_exception_when_accessing_unknown_flag(){
+
+        $this->withoutExceptionHandling();
+        $this->expectException(\App\Exceptions\UnknownFeatureException::class);
+        FeatureFlag::isEnabled('in_app_chat');
+        
+    }
+
+    public function test_can_enable_feature(){
+        config(['features.notification_history.enabled' => false]);
+        FeatureFlag::enable('notification_history');
+        $this->assertTrue(config('features.notification_history.enabled'));
+    }
+
+    public function test_can_disable_feature(){
+        config(['features.notification_history.enabled' => true]);
+        FeatureFlag::disable('notification_history');
+        $this->assertTrue(config(['features.notification_history.enabled']) == false);
+    }
+}
