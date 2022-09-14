de8de7b8938e2382b1d8c00698319eda5a763846
Sodeeq Elusoji <omoelu1@gmail.com>
2022-09-14 08:49:31 +0100

    prefix feature flags env variables with FEATURE_FLAG_

--- config/features.php
+++ config/features.php
@@ -2,23 +2,19 @@
 
 return [
     'odds' => [
-        'enabled' => env('ODDS_ENABLED', false),
+        'enabled' => env('FEATURE_FLAG_ODDS_ENABLED', false),
         'activate_on' => null //@TODO future consideration: making a feature activated beginning from a certain date
     ],
-    'question_hardening' => [
-        'enabled' => env('QUESTION_HARDENER_ENABLED', false),
-        'activate_on' => null
-    ],
     'phone_verification' => [
-        'enabled' => env('PHONE_VERIFICATION_ENABLED', false),
+        'enabled' => env('FEATURE_FLAG_PHONE_VERIFICATION_ENABLED', false),
         'activate_on' => null
     ],
     'game_staking' => [
-        'enabled' => env('GAME_STAKING_ENABLED', false),
+        'enabled' => env('FEATURE_FLAG_GAME_STAKING_ENABLED', false),
         'activate_on' => null
     ],
     'notification_history' => [
-        'enabled' => env('NOTIFICATION_HISTORY_ENABLED', false),
+        'enabled' => env('FEATURE_FLAG_NOTIFICATION_HISTORY_ENABLED', false),
         'activate_on' => '2022-09-13'
     ]
 ];
