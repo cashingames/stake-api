dd14cd24290a393a3ad5cf5688976233157f6aef
Sodeeq Elusoji <omoelu1@gmail.com>
2022-09-21 12:01:26 +0100

    create challengegame service

---
+++ app/Services/ChallengeGameService.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\User;
+use App\Models\Challenge;
+use App\Mail\ChallengeInvite;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
+use App\Actions\SendPushNotification;
+use App\Notifications\ChallengeReceivedNotification;
+
+class ChallengeGameService{
+
+    /**
+     * Create a new challenge and send invite notification to the opponent(s)
+     * 
+     * @param App\User $creator
+     * 
+     * @param mixed $opponent
+     * $opponent can either be a single id, or an array of ids. If an array of ids is supplied, notification will be sent to all opponents involved
+     * 
+     * @param int $categoryId
+     */
+    public function createChallenge($creator, $opponents, $categoryId){
+        if (is_numeric($opponents)){
+            $opponents = [$opponents];
+        }
+        $createdChallenges = [];
+        foreach ($opponents as $opponentId) {
+            $challenge = Challenge::create([
+                'status' => 'PENDING',
+                'user_id' => $creator->id,
+                'category_id' => $categoryId,
+                'opponent_id' => $opponentId
+            ]);
+
+            $opponent = User::find($opponentId);
+
+            //database notification
+            $opponent->notify(new ChallengeReceivedNotification($challenge, $this->user));
+            //email notification
+            Mail::send(new ChallengeInvite($opponent, $challenge->id));
+            //push notification
+            $pushAction = new SendPushNotification();
+            $pushAction->sendChallengeInviteNotification($creator, $opponent, $challenge);
+
+            Log::info("Challenge id : $challenge->id  invite from " . $creator->username . " sent to {$opponent->username}");
+            array_push($createdChallenges, $challenge);
+        }
+        return $createdChallenges;
+    }
+}
