e56903e792b9f18fb411ff2a11696b861c9ad0ef
Zubby Uwajimgba <zubbybrightson@gmail.com>
2022-09-30 11:20:03 +0100

    created migration

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -32,7 +32,7 @@ class Question extends Model
 
   public function category()
   {
-    return $this->belongsTo(Category::class);
+    return $this->belongsToMany(Category::class);
   }
 
   public function games()
