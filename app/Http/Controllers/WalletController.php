c041a56c71d891ba33bf813b9a209d17038209de
Oyesola Ogundele <oyekunmi@gmail.com>
2022-12-05 10:18:01 +0400

    make sensitive endpoint private

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -62,6 +62,7 @@ class WalletController extends BaseController
         if (!in_array($request->getClientIp(), ['52.31.139.75', '52.49.173.169', '52.214.14.220'])) {
             return response("", 200);
         }
+        Log::info("lient ip ". $request->getClientIp());
 
         $input = @file_get_contents("php://input");
         $event = json_decode($input);
