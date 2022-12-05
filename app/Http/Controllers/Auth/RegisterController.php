efcfd74a10441286e10df490127ee8a61b31270c
Zubby Uwajimgba <zubbybrightson@gmail.com>
2022-12-05 14:09:11 +0100

    rename phone number rule

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Auth\Events\Registered;
 use App\Http\Controllers\BaseController;
-use App\Rules\PhoneNumberRule;
+use App\Rules\UniquePhoneNumberRule;
 use App\Services\FeatureFlag;
 use Illuminate\Support\Facades\Validator;
 use App\Services\SMS\SMSProviderInterface;
@@ -71,7 +71,7 @@ class RegisterController extends BaseController
             'last_name' => ['required', 'string', 'max:255'],
             'username' => ['required', 'string', 'max:255', 'unique:users'],
             'country_code' => ['required', 'string', 'max:4'],
-            'phone_number' => ['required', 'numeric', new PhoneNumberRule], 
+            'phone_number' => ['required', 'numeric', new UniquePhoneNumberRule], 
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
             'referrer' => ['nullable', 'string', 'exists:users,username'],
--- app/Rules/UniquePhoneNumberRule.php
+++ app/Rules/UniquePhoneNumberRule.php
@@ -5,7 +5,7 @@ namespace App\Rules;
 use App\Models\User;
 use Illuminate\Contracts\Validation\Rule;
 
-class PhoneNumberRule implements Rule
+class UniquePhoneNumberRule implements Rule
 {
     /**
      * Create a new rule instance.
