4fddbd1ceb8d830c634d616bd55d66a651ac7791
Zubby Brightson Uwajimgba <39646315+zubbybright@users.noreply.github.com>
2023-01-23 12:13:38 +0100

    Update EndChallengeGameController.php

--- app/Http/Controllers/EndChallengeGameController.php
+++ app/Http/Controllers/EndChallengeGameController.php
@@ -31,7 +31,7 @@ class EndChallengeGameController extends  BaseController
         }
         //@TODO Remove after fixing double submission bug.
         if ($game->state === "COMPLETED") {
-            $game->minimum_boast_score = $this->MINIMUM_GAME_BOOST_SCORE;
+            $game->minimum_boost_score = $this->MINIMUM_GAME_BOOST_SCORE;
             return $this->sendResponse($game, 'Challenge Game Ended');
         }
         $game->end_time = Carbon::now()->subMilliseconds(500);
