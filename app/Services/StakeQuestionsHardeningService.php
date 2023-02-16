3e20ad710d558a841aac07cd0ba8b92145fd842c
Oyesola Ogundele <oyekunmi@gmail.com>
2023-02-16 18:22:49 +0400

    if henryjones hard and no repeat

--- app/Services/StakeQuestionsHardeningService.php
+++ app/Services/StakeQuestionsHardeningService.php
@@ -13,16 +13,41 @@ class StakeQuestionsHardeningService implements QuestionsHardeningServiceInterfa
 {
     public function determineQuestions(string $userId, string $categoryId, ?string $triviaId): Collection
     {
+        $user = auth()->user();
+
+        if ($user->username == 'HeneryJones') {
+            return $this->getHardQuestions($user, $categoryId);
+        }
+
         return $this->getEasyQuestions($categoryId);
     }
 
     private function getEasyQuestions(string $categoryId): Collection
     {
-
         return Category::find($categoryId)->questions()
             ->where('is_published', true)
             ->where('level', 'easy')
             ->inRandomOrder()->take(20)->get();
     }
 
+    private function getHardQuestions($user, string $categoryId): Collection
+    {
+
+        $recentQuestions = $this->getUserAnsweredQuestions($user);
+
+        return Category::find($categoryId)->questions()
+            ->where('is_published', true)
+            ->where('level', 'hard')
+            ->whereNotIn('questions.id', $recentQuestions)
+            ->inRandomOrder()->take(20)->get();
+    }
+
+    private function getUserAnsweredQuestions($user)
+    {
+        return $user
+            ->gameSessionQuestions()
+            ->latest('game_sessions.created_at')->take(1000)->pluck('question_id');
+    }
+
 }
+
