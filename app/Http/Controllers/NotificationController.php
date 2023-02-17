ef14d53dfe4c6e5d8224a0c9be82d8f34e19cd63
Zubby Uwajimgba <zubbybrightson@gmail.com>
2023-02-17 17:50:56 +0100

    remove challenge notifications for staking app

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -10,19 +10,31 @@ class NotificationController extends BaseController
     /**
      * Fetch all notifications
      */
-    public function index(Request $request){
+    public function index(Request $request)
+    {
+        if ($request->header('x-brand-id') == 2) {
+            $notifications = $this->user->notifications()->where('data', 'not like', '%CHALLENGE%')->paginate(20);
+            return $this->sendResponse($notifications, "Notifications fetched successfully");
+        }
         $notifications = $this->user->notifications()->paginate(20);
         return $this->sendResponse($notifications, "Notifications fetched successfully");
     }
     /**
      * Mark single or all user notifications as read
      */
-    public function readNotification($notificationId){
-        if ($notificationId == "all"){
-            foreach ($this->user->unreadNotifications as $notification) {
-                $notification->update(['read_at' => now()]);
+    public function readNotification(Request $request, $notificationId)
+    {
+        if ($notificationId == "all") {
+            if ($request->header('x-brand-id') == 2) {
+                foreach ($this->user->unreadNotifications()->where('data', 'not like', '%CHALLENGE%') as $notification) {
+                    $notification->update(['read_at' => now()]);
+                }
+            } else {
+                foreach ($this->user->unreadNotifications as $notification) {
+                    $notification->update(['read_at' => now()]);
+                }
             }
-        }else{
+        } else {
             UserNotification::whereId($notificationId)->update(['read_at' => now()]);
         }
 
