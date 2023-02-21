7cc2b15c3bebc7d3c3ff52d3b8839f1715d2650a
Zubby Uwajimgba <zubbybrightson@gmail.com>
2023-02-21 16:47:32 +0100

    drop category id from questions

--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -24,12 +24,7 @@ class QuestionFactory extends Factory
       'game_type_id' => function () {
         return GameType::inRandomOrder()->first()->id;
       },
-      'category_id' => function () {
-        return Category::where('category_id', '!=', 0)->inRandomOrder()->first()->id;
-      },
       'is_published' => true
-      // 'game_type_id' => $this->faker->randomElement(array(1,2)),
-      // 'category_id' => $this->faker->randomElement(array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11))
     ];
   }
 }
