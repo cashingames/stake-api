6b83af8f4b65d6a45dac570d668e653d7541be0a
Seyi_Coded <opadonuseyi01@gmail.com>
2023-02-23 19:21:36 +0100

    constant re-naming

--- config/trivia.php
+++ config/trivia.php
@@ -9,7 +9,7 @@ return [
       'referral' => env('REFERRER_SIGNUP_BONUS', false),
       'referral_on_first_game' => env('REFERRER_SIGNUP_BONUS_ON_FIRST_GAME', false),
       'referral_on_signup' => env('REFERRER_SIGNUP_BONUS_ON_REGISTRATION', false),
-      'referral_on_signup_bonus_amount' => env('REFERRER_SIGNUP_BONUS_AMOUNT', 2),
+      'referral_on_signup_bonus_amount' => env('REFERRER_SIGNUP_BONUS_GAMES_COUNT', 2),
     ],
   ],
   'tournament' => [
