7055b9955f85eaf0f432ab7acb46983fde53e1ba
Zubby Uwajimgba <zubbybrightson@gmail.com>
2023-02-27 08:59:19 +0100

    reduce random numbber on username to two

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -109,7 +109,7 @@ class RegisterController extends BaseController
             User::create([
                 'username' => $platform !== ClientPlatform::StakingMobileWeb
                     ? $data['username']
-                    :strstr($data['email'], '@', true). mt_rand(1000, 9999),
+                    :strstr($data['email'], '@', true). mt_rand(10, 99),
                 'phone_number' =>  str_starts_with($data['phone_number'], '0') ?
                     ltrim($data['phone_number'], $data['phone_number'][0]) : $data['phone_number'],
                 'email' => $data['email'],
