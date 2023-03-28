09845f247e929bcde600f7792745a7f1adb5cb46
Zubby Uwajimgba <zubbybrightson@gmail.com>
2023-03-28 13:33:32 +0100

    move match to backeground job

--- app/Jobs/ChallengeRequestMatch.php
+++ app/Jobs/ChallengeRequestMatch.php
@@ -2,6 +2,8 @@
 
 namespace App\Jobs;
 
+use App\Models\ChallengeRequest;
+use App\Actions\TriviaChallenge\MatchRequestAction;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -9,23 +11,23 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 
-class ChallengRequestMatch implements ShouldQueue
+class ChallengeRequestMatch implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     /**
      * Create a new job instance.
      */
-    public function __construct()
+    public function __construct(  public ChallengeRequest $match)
     {
-        //
+    
     }
 
     /**
      * Execute the job.
      */
-    public function handle(): void
+    public function handle(MatchRequestAction $matchAction,): void
     {
-        //
+        $matchAction->execute($this->match); 
     }
 }
