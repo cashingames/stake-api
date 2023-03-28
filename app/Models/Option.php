b234ba0ea07e2f3e9ac41656b3817db89841012d
Oyesola Ogundele <oyekunmi@gmail.com>
2023-03-28 15:23:02 +0400

    feat(questions): remove encryption

--- app/Models/Option.php
+++ app/Models/Option.php
@@ -7,33 +7,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class Option extends Model
 {
-  use HasFactory;
+    use HasFactory;
 
-  /**
-   * The attributes that should be hidden for arrays.
-   *
-   * @var array
-   */
-  protected $hidden = [
-    'question_id ', 'created_at', 'updated_at', 'is_correct'
-  ];
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'question_id ',
+        'created_at',
+        'updated_at',
+        'is_correct'
+    ];
 
-  protected $casts = [
-    // 'is_correct' => 'boolean',
-    // 'title' => 'string'
-  ];
+    protected $casts = [
+        // 'is_correct' => 'boolean',
+        // 'title' => 'string'
+    ];
 
-  public function question()
-  {
-    return $this->belongsTo(Question::class);
-  }
-
-  public function getIsCorrectAttribute($value)
-  {
-    return base64_encode($value);
-  }
-  public function getTitleAttribute($value)
-  {
-    return base64_encode(str_replace('"', '', $value));
-  }
+    public function question()
+    {
+        return $this->belongsTo(Question::class);
+    }
 }
