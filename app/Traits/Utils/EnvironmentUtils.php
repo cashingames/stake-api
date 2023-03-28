fe298c14b5d5c1b5a3c3dca24d5d94f018285b45
Oyesola Ogundele <oyekunmi@gmail.com>
2023-03-28 14:23:39 +0400

    feat(type): fix typo

--- app/Traits/Utils/EnvironmentUtils.php
+++ app/Traits/Utils/EnvironmentUtils.php
@@ -8,9 +8,9 @@ trait EnvironmentUtils
     static function setGoogleCredentials(): void
     {
         if (request()->header('x-request-env') == 'development') {
-            putenv('GOOGLE_APPLICATION_CREDENTIALS=' . base_path('google-credentials-dev.json'));
+            putenv('GOOGLE_APPLICATION_CREDENTIALS=' . storage_path('app/firebase/google-credentials-dev.json'));
         } else {
-            putenv('GOOGLE_APPLICATION_CREDENTIALS=' . base_path('google-credentials.json'));
+            putenv('GOOGLE_APPLICATION_CREDENTIALS=' . storage_path('app/firebase/google-credentials.json'));
         }
     }
 
