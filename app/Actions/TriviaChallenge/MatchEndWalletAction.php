13dbd732f3a179463b4031471bba8919d7a98d8f
Oyesola Ogundele <oyekunmi@gmail.com>
2023-04-01 10:35:23 +0400

    feat(bug): fix wrong casting in php8.1

--- app/Actions/TriviaChallenge/MatchEndWalletAction.php
+++ app/Actions/TriviaChallenge/MatchEndWalletAction.php
@@ -14,57 +14,75 @@ class MatchEndWalletAction
     ) {
     }
 
-    public function execute(string $requestId): ChallengeRequest|null|int
+    public function execute(string $requestId): ChallengeRequest|null
     {
         $winner = $this->getChallengeWinner(
             $request = $this->triviaChallengeStakingRepository->getRequestById($requestId),
             $matchedRequest = $this->triviaChallengeStakingRepository->getMatchedRequestById($requestId)
         );
 
-        if ($winner == -1) {
-            $this->creditWalletAction->execute(
-                $request->user->wallet,
-                $request->amount,
-                'Trivia challenge staking refund'
-            );
-
-            $this->creditWalletAction->execute(
-                $matchedRequest->user->wallet,
-                $matchedRequest->amount,
-                'Trivia challenge staking refund'
-            );
-
-        } elseif ($winner !== null) {
-            $amountWon = $winner->amount * 2;
-            $this->creditWalletAction->execute(
-                $winner->user->wallet,
-                $amountWon,
-                'Trivia challenge staking winning'
-            );
-            ChallengeRequest::where('challenge_request_id', $winner->challenge_request_id)
-                ->update([
-                    'amount_won' => $amountWon
-                ]);
+        if ($this->isCompleted($request, $matchedRequest) && $winner == null) {
+            $this->refundMatchedOpponents($request, $matchedRequest);
         }
 
+        if ($winner != null) {
+            $this->creditWinner($winner);
+        }
 
         return $winner;
+    }
+
+    private function refundMatchedOpponents(ChallengeRequest $request, ChallengeRequest $matchedRequest): void
+    {
+        $this->creditWalletAction->execute(
+            $request->user->wallet,
+            $request->amount,
+            'Trivia challenge staking refund'
+        );
+
+        $this->creditWalletAction->execute(
+            $matchedRequest->user->wallet,
+            $matchedRequest->amount,
+            'Trivia challenge staking refund'
+        );
+
+    }
+
+    private function creditWinner(ChallengeRequest $winner): void
+    {
+        $amountWon = $winner->amount * 2;
+        $this->creditWalletAction->execute(
+            $winner->user->wallet,
+            $amountWon,
+            'Trivia challenge staking winning'
+        );
+        ChallengeRequest::where('challenge_request_id', $winner->challenge_request_id)
+            ->update([
+                'amount_won' => $amountWon
+            ]);
     }
 
     private function getChallengeWinner(
         ChallengeRequest $request,
         ChallengeRequest $matchedRequest
-    ): ChallengeRequest|null|int {
+    ): ChallengeRequest|null {
 
-        if ($matchedRequest->status !== 'COMPLETED' || $request->status !== 'COMPLETED') {
+        if ($this->isCompleted($request, $matchedRequest)) {
             return null;
         }
 
-        if ($request->score === $matchedRequest->score) {
-            return -1;
+        if ($request->score == $matchedRequest->score) {
+            return null;
         }
 
         return $request->score > $matchedRequest->score ? $request : $matchedRequest;
+    }
+
+    private function isCompleted(
+        ChallengeRequest $request,
+        ChallengeRequest $matchedRequest
+    ): bool {
+        return $matchedRequest->status !== 'COMPLETED' && $request->status !== 'COMPLETED';
     }
 
 
