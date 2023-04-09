1155ff823f091f56a056a4a742128a3dac725864
Oyesola Ogundele <oyekunmi@gmail.com>
2023-04-09 07:02:50 +0400

    feat(challenge): remove environment for gc

--- app/Traits/Utils/EnvironmentUtils.php
+++ app/Traits/Utils/EnvironmentUtils.php
@@ -7,11 +7,7 @@ trait EnvironmentUtils
 
     static function setGoogleCredentials(string $env = ""): void
     {
-        if (request()->header('x-request-env') == 'development' || $env == 'development') {
-            putenv('GOOGLE_APPLICATION_CREDENTIALS=' . storage_path('app/firebase/google-credentials-dev.json'));
-        } else {
-            putenv('GOOGLE_APPLICATION_CREDENTIALS=' . storage_path('app/firebase/google-credentials.json'));
-        }
+        putenv('GOOGLE_APPLICATION_CREDENTIALS=' . storage_path('app/firebase/google-credentials.json'));
     }
 
 }
