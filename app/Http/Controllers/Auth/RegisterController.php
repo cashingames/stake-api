374f680167b30b7ec33da244080251ab14b9abf7
Seyi_Coded <opadonuseyi01@gmail.com>
2023-04-11 22:12:47 +0100

    adjustment for CA

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -192,12 +192,6 @@ class RegisterController extends BaseController
             'boost_count' => 3,
             'used_count' => 0
         ]);
-        $user->boosts()->create([
-            'user_id' => $user->id,
-            'boost_id' => Boost::where('name', 'Bomb')->first()->id,
-            'boost_count' => 3,
-            'used_count' => 0
-        ]);
     }
 
     protected function GamearkSingUpBonus($user)
