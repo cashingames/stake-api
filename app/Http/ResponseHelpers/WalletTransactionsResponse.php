dd8e2f7478da3e7a7edab076b8c6b89c7b9bf1d0
Zubby Uwajimgba <zubbybrightson@gmail.com>
2023-04-12 17:46:20 +0100

    return transactions in nigerian timezone

--- app/Http/ResponseHelpers/WalletTransactionsResponse.php
+++ app/Http/ResponseHelpers/WalletTransactionsResponse.php
@@ -3,11 +3,14 @@
 namespace App\Http\ResponseHelpers;
 
 use App\Enums\WalletTransactionType;
+use Carbon\Carbon;
 use \Illuminate\Http\JsonResponse;
 use stdClass;
+use App\Traits\Utils\DateUtils;
 
 class WalletTransactionsResponse
 {   
+    use DateUtils;
     public int $transactionId;
     public WalletTransactionType $type;
     public $amount;
@@ -29,7 +32,7 @@ class WalletTransactionsResponse
             $transaction->type = $this->getTransactionType($t->type);
             $transaction->amount = $t->amount;
             $transaction->description = $t->description;
-            $transaction->transactionDate = $t->transactionDate;
+            $transaction->transactionDate = $this->toNigeriaTimeZoneFromUtc($t->transactionDate)->toDateTimeString();
 
             $presenter[] = $transaction;
         }
