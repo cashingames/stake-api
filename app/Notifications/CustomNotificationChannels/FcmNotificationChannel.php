1f8d818f61bcee2fa262807883516f9995914546
Zubby Uwajimgba <zubbybrightson@gmail.com>
2023-05-03 12:39:53 +0100

    creating custom channel

---
+++ app/Notifications/CustomNotificationChannels/FcmNotificationChannel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Notifications\CustomNotificationChannels;
+
+use Illuminate\Notifications\Notification;
+
+class FcmNotificationChannel
+{
+    public function send ($notifiable, Notification $notification) {
+        
+    }
+}
