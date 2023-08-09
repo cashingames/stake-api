f88fcda596d5e58dcf18665981a7671de774a9f5
Zubby Uwajimgba <zubbybrightson@gmail.com>
2023-08-09 18:21:05 +0100

    extra test for paystac service

--- tests/Unit/PaymentServiceTest.php
+++ tests/Unit/PaymentServiceTest.php
@@ -13,37 +13,110 @@ use Tests\TestCase;
 
 class PaymentServiceTest extends TestCase
 {
-    public $paymentService;
+    public $paymentService, $bankCode;
+    public $accountNumber, $accountName, $banksMock;
     protected function setUp(): void
     {
         parent::setUp();
         config(['trivia.payment_key' => 'sk_test_f6d2a64925009ef4b4544c90b96ddcb3991fa0c3']);
         $this->mockGuzzleClient();
-        $this->paymentService = new PaystackService();
+        $this->paymentService = $this->mockPaystackService();
+        $this->bankCode = '078';
+        $this->accountNumber = '1234567890';
+        $this->accountName = 'Account Name';
+        $this->banksMock = json_decode(json_encode([
+            'data' => [
+                [
+                    'name' => 'Test Bank',
+                    'code' => "059"
+                ]
+            ]
+        ]));
+        config(['trivia.payment_key' => 'sk_test_f6d2a64925009ef4b4544c90b96ddcb3991fa0c3']);
     }
 
     public function test_that_account_can_be_verified()
     {
-        $result =  $this->paymentService->verifyAccount('1234567890', '078');
+        $data = [
+            'status' => true,
+            'data' => (object) [
+                'account_name' => $this->accountName
+            ]
+        ];
+        $this->paymentService->expects($this->once())
+            ->method('verifyAccount')
+            ->with($this->accountNumber,  $this->bankCode)
+            ->willReturn($data);
+
+        $result =  $this->paymentService->verifyAccount($this->accountNumber,  $this->bankCode);
+        $this->assertEquals($result['data']->account_name, $this->accountName);
+    }
+
+    public function test_that_verify_account_can_fail()
+    {   
+        $service = new PaystackService();
+        $result =  $service->verifyAccount($this->accountNumber,  $this->bankCode);
 
         $this->assertIsObject($result);
     }
 
-    public function test_that_transfer_recipient_can_be_created()
+    public function test_that_banks_can_fail()
+    {   
+        $service = new PaystackService();
+        $result = $service->getBanks();
+
+        $this->assertIsObject($result);
+    }
+
+
+    public function test_that_transfer_recipient_can_return_null()
     {
-        $result =  $this->paymentService->createTransferRecipient('078', 'Account Name', '1234567890');
+        $result =  $this->paymentService->createTransferRecipient($this->bankCode, $this->accountName, $this->accountNumber);
 
         $this->assertEquals($result, null);
     }
 
+
+    public function test_that_transfer_recipient_can_be_created()
+    {
+        $this->paymentService->expects($this->once())
+            ->method('createTransferRecipient')
+            ->with($this->bankCode, $this->accountName, $this->accountNumber)
+            ->willReturn('randomrecipientcode');
+
+        $result =  $this->paymentService->createTransferRecipient($this->bankCode, $this->accountName, $this->accountNumber);
+
+        $this->assertEquals($result, 'randomrecipientcode');
+    }
+
     public function test_that_banks_can_be_fetched()
     {
+        $this->paymentService->expects($this->once())
+            ->method('getBanks')
+            ->willReturn($this->banksMock);
+
         $result = $this->paymentService->getBanks();
 
-        $this->assertIsObject($result);
+        $this->assertEquals($result, $this->banksMock);
     }
 
     public function test_transfer_can_be_initiated()
+    {
+        $data = (object)[
+            'status' => 'success',
+            'reference' => 'randomref'
+        ];
+        $this->paymentService->expects($this->once())
+            ->method('initiateTransfer')
+            ->with('randomrecipientcode', 200)
+            ->willReturn($data);
+
+        $result = $this->paymentService->initiateTransfer('randomrecipientcode', 200);
+
+        $this->assertEquals($result, $data);
+    }
+
+    public function test_transfer_initiated_url_can_be_called()
     {
         $mock = new MockHandler([
             new Response(200, [
@@ -69,8 +142,79 @@ class PaymentServiceTest extends TestCase
         $this->assertEquals($response->getStatusCode(), 200);
     }
 
+    public function test_verify_account_url_can_be_called()
+    {
+        $mock = new MockHandler([
+            new Response(200, [
+                'status' => true,
+                'data' =>  [
+                    'account_name' => $this->accountName
+                ]
+            ]),
+        ]);
+
+        $handlerStack = HandlerStack::create($mock);
+        $client = new Client(['handler' => $handlerStack]);
+
+        $url = "https://api.paystack.co/bank/resolve?account_number=" . $this->accountNumber . "&bank_code=" . $this->bankCode;
+
+        $response = $client->request('GET', $url);
+
+        $this->assertEquals($response->getStatusCode(), 200);
+    }
+
+    public function test_transfer_recipient_url_can_be_called()
+    {
+        $mock = new MockHandler([
+            new Response(200, ['recipientCode']),
+        ]);
+
+        $handlerStack = HandlerStack::create($mock);
+        $client = new Client(['handler' => $handlerStack]);
+
+        $url = "https://api.paystack.co/transferrecipient";
+
+        $response = $client->request('POST', $url, [
+            'json' => [
+                "type" => "nuban",
+                "name" => $this->accountName,
+                "account_number" => $this->accountNumber,
+                "bank_code" => $this->bankCode,
+                "currency" => "NGN"
+            ]
+        ]);
+
+        $this->assertEquals($response->getStatusCode(), 200);
+    }
+
+    public function test_get_banks_url_can_be_called()
+    {
+        $mock = new MockHandler([
+            new Response(200,  [
+
+                'name' => 'Test Bank',
+                'code' => "059"
+
+            ]),
+        ]);
+
+        $handlerStack = HandlerStack::create($mock);
+        $client = new Client(['handler' => $handlerStack]);
+
+        $url = 'https://api.paystack.co/bank';
+
+        $response = $client->request('GET', $url);
+
+        $this->assertEquals($response->getStatusCode(), 200);
+    }
+
     private function mockGuzzleClient()
     {
         return $this->createMock(Client::class);
+    }
+
+    private function mockPaystackService()
+    {
+        return $this->createMock(PaystackService::class);
     }
 }
