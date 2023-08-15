233473d3fffdf0b5095f976b5f1768e7d9a5e681
Zubby Uwajimgba <zubbybrightson@gmail.com>
2023-08-15 13:06:31 +0100

    cash drops resource controller

--- routes/api.php
+++ routes/api.php
@@ -20,8 +20,8 @@ use App\Http\Controllers\Auth\ForgotPasswordController;
 use App\Http\Controllers\RegisterPushDeviceTokenController;
 use App\Http\Controllers\PlayGame\EndChallengeGameController;
 use App\Http\Controllers\BoostsController;
+use App\Http\Controllers\CashdropsController;
 use App\Http\Controllers\GetBonusOddsController;
-use App\Http\Controllers\GetCashdropsController;
 use App\Http\Controllers\GetUserTransactionsController;
 use App\Http\Controllers\PlayGame\EndPracticeChallengeGameController;
 use App\Http\Controllers\PlayGame\EndSinglePlayerPracticeGameController;
@@ -92,6 +92,6 @@ Route::middleware(['auth:api'])->prefix('v3')->group(
         Route::post('challenges/practice/create', StartPracticeChallengeRequestController::class);
         Route::post('challenges/practice/submit', EndPracticeChallengeGameController::class);
         Route::get('wallet/transactions/{wallet_type}', GetUserTransactionsController::class);
-        Route::get('cashdrops', GetCashdropsController::class);
+        Route::resource('cashdrops', CashdropsController::class);
     }
 );
