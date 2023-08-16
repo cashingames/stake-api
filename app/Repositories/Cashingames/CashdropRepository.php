b96aa7cfd204e0ffacb85a9e9ae01f53d17b74b3
Zubby Uwajimgba <zubbybrightson@gmail.com>
2023-08-16 13:44:27 +0100

    add background colour to cashdrops

--- app/Repositories/Cashingames/CashdropRepository.php
+++ app/Repositories/Cashingames/CashdropRepository.php
@@ -22,7 +22,7 @@ class CashdropRepository
         return DB::select(
             'SELECT cashdrops.name as cashdropName, cashdrops.id 
             as cashdropId, cashdrop_rounds.pooled_amount 
-            as pooledAmount from cashdrops 
+            as pooledAmount, cashdrops.background_colour as backgroundColor from cashdrops 
             left join cashdrop_rounds on cashdrops.id = cashdrop_rounds.cashdrop_id 
             where cashdrop_rounds.dropped_at is null
             order by cashdrops.lower_pool_limit DESC',
@@ -33,7 +33,7 @@ class CashdropRepository
     {
         return DB::select(
             'SELECT profiles.first_name, profiles.last_name, profiles.avatar, cashdrops.icon , cashdrops.name as cashdropsName,
-            cashdrop_rounds.id as cashdropRoundId, cashdrop_rounds.pooled_amount as pooledAmount FROM profiles 
+            cashdrops.background_colour as backgroundColor, cashdrop_rounds.id as cashdropRoundId, cashdrop_rounds.pooled_amount as pooledAmount FROM profiles 
             LEFT JOIN cashdrop_users on cashdrop_users.user_id = profiles.user_id
             LEFT JOIN cashdrop_rounds on cashdrop_users.cashdrop_round_id = cashdrop_rounds.id
             LEFT JOIN cashdrops on cashdrops.id = cashdrop_rounds.cashdrop_id
