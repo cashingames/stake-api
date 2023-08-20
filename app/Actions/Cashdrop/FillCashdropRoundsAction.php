617ff3d3f77c1eca77a32db1de8952aa4141f15a
Zubby Uwajimgba <zubbybrightson@gmail.com>
2023-08-20 17:53:23 +0100

    update firestore with cashdrop details

--- app/Actions/Cashdrop/FillCashdropRoundsAction.php
+++ app/Actions/Cashdrop/FillCashdropRoundsAction.php
@@ -4,20 +4,22 @@ namespace App\Actions\Cashdrop;
 
 use App\Models\User;
 use App\Repositories\Cashingames\CashdropRepository;
+use App\Services\Firebase\FirestoreService;
 use Illuminate\Support\Facades\DB;
 
 class FillCashdropRoundsAction
 {
     public function __construct(
         private readonly CashdropRepository $cashdropRepository,
+        private readonly FirestoreService $firestoreService,
     ) {
     }
 
     public function execute(User $user, float $amount)
     {
-
-        DB::transaction(function () use ($user, $amount) {
-            $this->cashdropRepository->getActiveCashdrops()->map(function ($round) use ($user, $amount) { 
+        $activeCashdrops = $this->cashdropRepository->getActiveCashdrops();
+        DB::transaction(function () use ($user, $amount, $activeCashdrops) {
+            $activeCashdrops->map(function ($round) use ($user, $amount) {
                 $this->cashdropRepository->updateUserCashdropRound(
                     $user->id,
                     $amount,
@@ -25,5 +27,21 @@ class FillCashdropRoundsAction
                 );
             });
         });
+
+        $this->updateCashdropFirestore($activeCashdrops);
+    }
+
+    private function updateCashdropFirestore($activeCashdrops)
+    {
+        foreach ($activeCashdrops as $cashdropsRound) {
+            $this->firestoreService->updateDocument(
+                'cashdrops-updates',
+                config('trivia.cashdrops_firestore_document_id'),
+                [
+                    'cashdrop_id' => $cashdropsRound->cashdrop_id,
+                    'cashdrop_amount' => $cashdropsRound->pooled_amount
+                ]
+            );
+        }
     }
 }
